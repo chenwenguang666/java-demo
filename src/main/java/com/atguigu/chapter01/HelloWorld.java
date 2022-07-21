87fc2b2bc13eca4cadf06d1b62a957a057e2730c
tom <tom@qq.com>
2022-07-21 23:57:55 +0800

    上传测试

---
+++ src/main/java/com/atguigu/chapter01/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.atguigu.chapter01;
+
+/**
+ * TODO 你好，世界   你好，Java
+ * Created by cwg on 2022/4/8
+ */
+public class HelloWorld {
+
+    public static void main(String[] args){
+
+        System.out.println("你好，世界！\n你好，java！");
+    }
+
+}
---
+++ src/main/java/com/atguigu/chapter02/VariableTest1.java
@@ -0,0 +1,31 @@
+package com.atguigu.chapter02;
+
+/**
+ * TODO 数据类型演示
+ * Created by cwg on 2022/4/14
+ */
+public class VariableTest1 {
+
+    public static void main(String[] args) {
+
+        // 基本数据类型
+
+        // 1) 数值型 -> 整数类型
+        // byte、short、int、long
+
+        // byte b1 = 128; // 报错，不能定义超出这个数据类型的值
+        byte b2 = 12;
+
+        short s1 = 15;
+        // 通常定义整型变量：int
+        int i1 = 22;
+
+        long l1 = 123889; // 为什么long型没加L还不会报错，因为这里涉及自动类型转换
+        long l2 = 1238893424242L;
+
+        // 2) 数值型 -> 浮点类型
+        // float、doubleb '
+
+
+    }
+}
